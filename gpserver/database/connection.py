345cd38e0eba6028054d0a85709f17f54b813e6f
Julian Syndercombe Steyn <yujisteyn@gmail.com>
2023-11-09 00:56:58 +1100

    fix configs for db engine

--- gpserver/database/connection.py
+++ gpserver/database/connection.py
@@ -8,4 +8,4 @@ engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(bind=engine)
